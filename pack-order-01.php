50285703c8de58085ca96abe383d898674e8281b
Adam Alyan <a.alyan@teknowl.net>
2020-07-19 17:56:27 +0100

    no message

--- pack-order-01.php
+++ pack-order-01.php
@@ -11,35 +11,98 @@ arsort($arrayPacks);
 $tests = [1, 250, 251, 501, 12001, 15251];
 
 foreach($tests as $test){
-  $packsToSend = efficientPackSizes($test, $arrayPacks);
+  $packsToSend = packOrder($test, $arrayPacks);
   echo "Testing: " . $test . "\n";
   print_r($packsToSend);
   echo "\n \n";
 }
 
 // Main function 
-function efficientPackSizes($requestNoWidgets, $arrayPacks = array()){
+function packOrder($requestNoWidgets, $arrayPacks){
   $packsToSend = array();
   $remainingWidgets = $requestNoWidgets;
     
   while($remainingWidgets > 0){
     
-     $nextpacksize = getNextPackSize($remainingWidgets, $arrayPacks);
-     $lastpacksize = $nextpacksize;
-     $remainingWidgets = $remainingWidgets - $nextpacksize;
-     $packsToSend[] =  $nextpacksize;
+    $nextpacksize = getNextPackSize2($remainingWidgets, $arrayPacks);
+    $lastpacksize = $nextpacksize;
+    $remainingWidgets = $remainingWidgets - $nextpacksize;
+    
+        
+    if(array_key_exists($nextpacksize, $packsToSend)) { 
+      $packsToSend[$nextpacksize] = $packsToSend[$nextpacksize] +1;
+    } else{
+      $packsToSend[$nextpacksize] = 1;
+    }
   } 
   
   return $packsToSend;
-  
 }
 
 
-function getNextPackSize($remainingWidgets, $arrayPacks){
+// Get the correct next pack size for the order
+// Loop through array packs desc. 
+// 
+function getNextPackSize2($remainingWidgets, $arrayPacks){  
   foreach($arrayPacks as $key => $size){
+    echo "size is: " . $size . "\n";
     $onePackSmallerKey = $key-1;
     $twoPackSmallerKey = $key-2;
-    //if ( $onePackSmallerKey > 0 && $twoPackSmallerKey >=0 ){
+  
+    if ( $onePackSmallerKey >= 0 ){
+      $onePackSmallerSize = $arrayPacks[$onePackSmallerKey];
+      $packDiff = $size - $onePackSmallerSize;
+      
+      if ( $twoPackSmallerKey >= 0 ){
+        $twoPackSmallerSize = $arrayPacks[$twoPackSmallerKey];
+        $SumOfSizeOfTwoSmallerPacks = $onePackSmallerSize + $twoPackSmallerSize;
+        
+        // check for two many packs
+        // if:
+        // 1) This size is not the smallest
+        // 2) (remaining number) is smaller than current pack size 
+        // 3) (remaining number) is bigger than the difference between the (current size) and (the one size below)
+        // 4) (remaining number) can be satisfied by the sum of the two smaller packs
+        if($remainingWidgets <= $size && $remainingWidgets > $packDiff && $remainingWidgets < $SumOfSizeOfTwoSmallerPacks ){
+          echo "1st if \n";
+          return $onePackSmallerSize;
+        }              
+      }    
+      
+      // check for two many widget & too many packs 
+      // if:
+      // 1) This size is not the smallest
+      // 2) (remaining number) is smaller than current pack size 
+      // 3) (remaining number) is bigger than the difference between the (current size) and (the one size below)      
+      if($remainingWidgets <= $size && $remainingWidgets > $packDiff){
+        echo "2nd if \n";
+        return $size;
+      }
+    }    
+    
+    // Return the biggest as long as the remaining widgets is bigger than the biggest pack
+    if($remainingWidgets >= $size){
+      echo "3rd if \n";
+      return $size;
+    }
+    
+    // set the size to the smallest pack if all the above conditions are not met
+    $leastPackSize = $size;
+  }
+  
+  echo "4th Condition \n";
+  return $leastPackSize;
+}
+
+
+
+
+// Get the correct next pack size for the order
+function getNextPackSize($remainingWidgets, $arrayPacks){
+  foreach($arrayPacks as $key => $size){
+    
+    $onePackSmallerKey = $key-1;
+    $twoPackSmallerKey = $key-2;
     if ( $onePackSmallerKey >= 0 ){
       $onePackSmallerSize = $arrayPacks[$onePackSmallerKey];
       $packDiff = $size - $onePackSmallerSize;
@@ -50,27 +113,14 @@ function getNextPackSize($remainingWidgets, $arrayPacks){
         if($remainingWidgets <= $size && $remainingWidgets > $packDiff && $remainingWidgets < $SumOfSizeOfTwoSmallerPacks ){
           echo "1st if \n";
           return $onePackSmallerSize;
-        }
-              
+        }              
       }
-
-      
-      
+ 
       if($remainingWidgets <= $size && $remainingWidgets > $packDiff){
         echo "2nd if \n";
         return $size;
       }
     }    
-    
-    // $smallerPackKey = $key-1;    
-    // if ( $smallerPackKey>= 0){
-    //   $smallerPackSize = $arrayPacks[$smallerPackKey];
-    //   $packDiff = $size - $smallerPackSize;
-    //   if($remainingWidgets <= $size && $remainingWidgets > $packDiff ){
-    //     echo "1st if \n";
-    //     return $size;
-    //   }
-    // }
     
     if($remainingWidgets >= $size){
       echo "3rd if \n";
@@ -79,6 +129,8 @@ function getNextPackSize($remainingWidgets, $arrayPacks){
     
     $leastPackSize = $size;
   }
+  
+  echo "4th Condition \n";
   return $leastPackSize;
 }
 
