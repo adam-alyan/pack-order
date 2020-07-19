ec8d6f3909b96faec3289c2f25950a75f6e1eac6
Adam Alyan <a.alyan@teknowl.net>
2020-07-19 17:57:25 +0100

    no message

--- pack-order-01.php
+++ pack-order-01.php
@@ -11,11 +11,93 @@ arsort($arrayPacks);
 $tests = [1, 250, 251, 501, 12001, 15251];
 
 foreach($tests as $test){
-  $packsToSend = packOrder($test, $arrayPacks);
   echo "Testing: " . $test . "\n";
+  //$packsToSend = packOrder_refactored($test, $arrayPacks);
+  $packsToSend = packOrder($test, $arrayPacks);
+  
   print_r($packsToSend);
   echo "\n \n";
 }
+
+
+function packOrder_refactored($requestNoWidgets, $arrayPacks){
+  $packsToSend = array();
+  
+  // number of widgets smaller than the smallest pack
+  $smallestPack = end($arrayPacks);                    
+  if($requestNoWidgets <= $smallestPack){
+    $packsToSend[$smallestPack] = 1;
+    return $packsToSend;
+  }
+  
+  $remainingWidgetsToPack = $requestNoWidgets;
+  
+  foreach($arrayPacks as $key => $packsize){
+    
+    // skip if pack size wrongly set to a negative number
+    if($packsize <= 0)continue;
+    
+    // skip if pack is too big
+    $remainder = $requestNoWidgets % $packsize;    
+    if($remainder === $remainingWidgetsToPack)continue;
+    
+    
+    if(array_key_exists($key-1, $arrayPacks)){
+      $diff = $packsize - $arrayPacks[$key-1];
+    }
+    
+//     //check if two smaller packs is more effeicent (too many widgets))      
+//     if(array_key_exists($key-2, $arrayPacks)){
+//       echo "2nd if \n";
+//       $sumOfTwoSmallerPacks = $arrayPacks[$key-1] + $arrayPacks[$key-2];
+//       //$diff = $packsize - $arrayPacks[$key-1];
+//       if ($remainingWidgetsToPack < $packsize && $remainingWidgetsToPack < $sumOfTwoSmallerPacks  ){
+//         $packsToSend = addPack($arrayPacks[$key-1] , $packsToSend);
+//         $packsToSend = addPack($arrayPacks[$key-2] , $packsToSend);
+//         $remainingWidgetsToPack = $remainingWidgetsToPack - $sumOfTwoSmallerPacks;
+//       }
+//     }    
+    
+    if($remainder > 1){
+      if($remainingWidgetsToPack > $packsize ){
+        echo "division: " . $remainingWidgetsToPack / $packsize . "\n";
+        echo "division: " . intdiv($remainingWidgetsToPack, $packsize) . "\n";
+        $numOfPackstoAdd = intdiv($remainingWidgetsToPack, $packsize);
+        $packsToSend = addPack($packsize , $packsToSend, $numOfPackstoAdd);
+        $remainingWidgetsToPack = $remainingWidgetsToPack - ($packsize * $numOfPackstoAdd);
+        echo "3rd if add " . $packsize . " x " . $numOfPackstoAdd .  "\n";
+        continue;
+      }
+    }
+    
+    // add the bigger pack to order if the size is between two packs
+    if(array_key_exists($key+1, $arrayPacks)){    
+      if($remainingWidgetsToPack > $packsize && $remainingWidgetsToPack < $arrayPacks[$key+1] && $remainingWidgetsToPack > $diff ){      
+        $packsToSend = addPack($arrayPacks[$key+1] , $packsToSend);
+        $remainingWidgetsToPack = $remainingWidgetsToPack - $arrayPacks[$key+1];
+        echo "4th if add " . $arrayPacks[$key+1] . "\n";
+        continue;
+      }
+    }
+    
+    
+
+ 
+    
+    echo $requestNoWidgets . " % " . $packsize  . " remainder = " . $remainder . "\n";
+  }
+  return $packsToSend;
+}
+
+function addPack($pack, $packsToSend, $number = 1){
+  if(array_key_exists($pack, $packsToSend)) { 
+    $packsToSend[$pack] = $packsToSend[$pack] +$number;
+  } else{
+    $packsToSend[$pack] = $number;
+  }
+  return $packsToSend;
+}
+
 
 // Main function 
 function packOrder($requestNoWidgets, $arrayPacks){
@@ -45,7 +127,7 @@ function packOrder($requestNoWidgets, $arrayPacks){
 // 
 function getNextPackSize2($remainingWidgets, $arrayPacks){  
   foreach($arrayPacks as $key => $size){
-    echo "size is: " . $size . "\n";
+    //echo "size is: " . $size . "\n";
     $onePackSmallerKey = $key-1;
     $twoPackSmallerKey = $key-2;
   
@@ -64,7 +146,7 @@ function getNextPackSize2($remainingWidgets, $arrayPacks){
         // 3) (remaining number) is bigger than the difference between the (current size) and (the one size below)
         // 4) (remaining number) can be satisfied by the sum of the two smaller packs
         if($remainingWidgets <= $size && $remainingWidgets > $packDiff && $remainingWidgets < $SumOfSizeOfTwoSmallerPacks ){
-          echo "1st if \n";
+          //echo "1st if \n";
           return $onePackSmallerSize;
         }              
       }    
@@ -75,14 +157,14 @@ function getNextPackSize2($remainingWidgets, $arrayPacks){
       // 2) (remaining number) is smaller than current pack size 
       // 3) (remaining number) is bigger than the difference between the (current size) and (the one size below)      
       if($remainingWidgets <= $size && $remainingWidgets > $packDiff){
-        echo "2nd if \n";
+        //echo "2nd if \n";
         return $size;
       }
     }    
     
     // Return the biggest as long as the remaining widgets is bigger than the biggest pack
     if($remainingWidgets >= $size){
-      echo "3rd if \n";
+      //echo "3rd if \n";
       return $size;
     }
     
@@ -90,7 +172,7 @@ function getNextPackSize2($remainingWidgets, $arrayPacks){
     $leastPackSize = $size;
   }
   
-  echo "4th Condition \n";
+  //echo "4th Condition \n";
   return $leastPackSize;
 }
 
